a7330a224cff5ae61bf3ac02acac00ebb64d1f6c
Matheus <mt.schreiber01@gmail.com>
2023-03-04 15:31:46 -0300

    :sparkles: add mobile frontend

---
+++ frontend-bet-app/src/theme/index.ts
@@ -0,0 +1,17 @@
+export const THEME = {
+  COLORS: {
+    LIGHT_GRAY: "#EFF4F5",
+    NEUTRAL_GRAY: "#F0F0F0",
+    GRAY: "#D9D9D9",
+    DARK_GRAY: "#898989",
+
+    GREEN: "#50B94E",
+    DARK_GREEN: "#448742",
+    LIGHT_GREEN: "#F1FFE2",
+
+    BLUE: "#399FFD",
+    LIGHT_BLUE: "#E2FAFF",
+
+    LIGHT_YELLOW: "#FFF7E3",
+  },
+};
